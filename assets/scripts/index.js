303f03851d306584d71b99dca5fd92a664cd6e72
philschoof <phil.schoof@gmail.com>
2017-09-13 15:11:30 -0400

    Renames keys in templates for clarity

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,6 @@ const eventHandlers = require('./event-handlers')
 $(() => {
   dataStore.loadJSONData
   .then(() => {
-    console.log(dataStore.data)
     eventHandlers.addHandlers() //must wait until data is loaded to render templates and add event handlers
   })
 })
